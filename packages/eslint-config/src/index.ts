c7c7f61d52c6f3ee98cc86981c147858108a0a9a
Andreas Storesund Madsen <andreas@asmadsen.no>
2021-07-25 15:04:15 +0200

    Initial commit

---
+++ packages/eslint-config/src/index.ts
@@ -0,0 +1,69 @@
+import type {TSESLint} from '@typescript-eslint/experimental-utils'
+
+const strict =
+  process.env.PRE_COMMIT || process.env.NODE_ENV?.toLowerCase() === 'production'
+
+const config: TSESLint.Linter.Config =  {
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  plugins: [
+    'promise',
+    '@typescript-eslint/eslint-plugin',
+    'prettier',
+    'import',
+  ],
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  reportUnusedDisableDirectives: true,
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': strict ? 'error' : 'off',
+    'no-debugger': strict ? 'error' : 'off',
+    'import/no-default-export': 'error',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/prefer-read-only-props': 'error',
+    'react/self-closing-comp': 'error',
+    'react/jsx-boolean-value': 'error',
+    'react/jsx-sort-props': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['.eslintrc.js', 'wallaby.config.js'],
+      parser: 'espree',
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+}
+
+module.exports = config
---
+++ packages/prettier-config/README.md
@@ -0,0 +1,3 @@
+# prettier-config
+
+This library was generated with [Nx](https://nx.dev).
